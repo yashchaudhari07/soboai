2b872fce5a5da45b3bedd022862a388d60aa72d3
yashchaudhari07 <yc98908277@gmail.com>
2025-12-09 19:07:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import DashboardLayout from "./layouts/DashboardLayout";
 import Home from "./pages/Dashboard/Home";
-import SurveyScreen from "./pages/Onboarding/SurveyScreen";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/dashboard" element={<DashboardLayout />} />
-
-        <Route path="/soboai-project" element={<Home />} />
       </Routes>
     </Router>
   );
